0086dd674708567fc493f65906b39d120f683e9c
Talisson Oliveira <talissonlux@outlook.com>
2021-10-06 20:28:16 -0300

    feat: reset css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <h1>Hello Wolrd</h1>
+    <>
+      <Head>
+        <title>Music Climate</title>
+      </Head>
+      <h1>Hello Wolrd</h1>
+    </>
   )
 }
