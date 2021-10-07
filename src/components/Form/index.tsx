8e58e526486acded624d995308a30da253e49cfe
Talisson Oliveira <talissonlux@outlook.com>
2021-10-07 04:23:36 -0300

    feat: get data from the weather api

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from 'react'
+
 import styles from './styles.module.scss'
 
+import { api } from '../../services/api'
+
+interface ResponseData {
+    main: {
+        temp: number
+    }
+    name: string
+}
+
 export function Form() {
+    const [weather, setWeather] = useState<ResponseData>()
+
+    useEffect(() => {
+        console.log(process.env.NEXT_PUBLIC_API_KEY)
+        api.get<ResponseData>('weather', {
+            params: {
+                q: 'Londres',
+                units: 'metric',
+                appid: process.env.NEXT_PUBLIC_API_KEY
+            }
+        })
+            .then(response => setWeather(response.data))
+    }, [])
+
+    useEffect(() => {
+        if (weather !== undefined) {
+            console.log(weather.main)
+        }
+    }, [weather])
+
     return (
         <form className={styles.form}>
             <input
