f8f7e719952051699d8f5b2e3542e53934d10f58
Talisson Oliveira <talissonlux@outlook.com>
2021-10-08 20:29:38 -0300

    feat: create SavePlaylistButton component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
                 </Head>
 
                 <body>
