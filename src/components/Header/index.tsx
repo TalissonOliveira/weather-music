b04abf638b94d69d837f4adf4871e71737e7d6c8
Talisson Oliveira <talissonlux@outlook.com>
2021-10-08 19:45:46 -0300

    refactor: change header title

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export function Header() {
     return (
         <header className={styles.headerContainer} >
             <div className={styles.headerContent}>
-                <h1>Weather Music</h1>
+                <h1>Encontre a playlist <br/>ideal para o clima do seu dia! :)</h1>
             </div>
         </header>
     )
